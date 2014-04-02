359bfcd3a1e72925f8987af36f145cc1c64af459
Kevin Campusano <megakevinx@gmail.com>
2014-04-02 02:35:28 -0400

    Obtained all tags and ordered them by their associated commit's date.

--- get_contrib_per_tag.py
+++ get_contrib_per_tag.py
@@ -4,39 +4,37 @@
 # and extracts the file-level contribution information using
 # the get_top_contrib_per_file script.
 
+
 __author__ = 'kevin'
 
+from datetime import datetime
 import json
 import os
-
 import pygit2
 
 git_folder = ".git/"
+
+
+class GitObjectType():
+    """Defines Git object types in terms of pygiy2's GIT_OBJs"""
+
+    commit = pygit2.GIT_OBJ_COMMIT
+    tag = pygit2.GIT_OBJ_TAG
+
 
 def main(git_repo):
     repo_path = os.path.join(git_repo, git_folder)
     repo = pygit2.Repository(repo_path)
 
-    objects = {
-        'tags': []
-    }
+    tags = [{'name': repo[obj_hex].name,
+             'date': str(datetime.fromtimestamp(repo[obj_hex].get_object().commit_time))}
+            for obj_hex in repo if repo[obj_hex].type == GitObjectType.tag]
 
-    for objhex in repo:
-        obj = repo[objhex]
-        if obj.type == pygit2.GIT_OBJ_TAG:
-            objects['tags'].append({
-                'hex': obj.hex,
-                'name': obj.name,
-                'message': obj.message,
-                'target': obj.target.hex, # base64.b16encode(obj.target).lower(),
-                'tagger_name': obj.tagger.name,
-                'tagger_email': obj.tagger.email,
-            })
-        else:
-            # ignore blobs and trees
-            pass
+    tags.sort(key=lambda t: t['date'])
 
-    print(json.dumps(objects, indent=2))
+    
+
+    print(json.dumps(tags, indent=2))
 
 
 if __name__ == '__main__':
