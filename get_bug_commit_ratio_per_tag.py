bf5fb971df57ef9110345e6968d0e988ac573f0d
Kevin Campusano <megakevinx@gmail.com>
2014-05-03 00:34:08 -0400

    bug and commit ration per tag rewritten to work with dates intstead of revisions, efectively flatting the revision tree.

--- get_bug_commit_ratio_per_tag.py
+++ get_bug_commit_ratio_per_tag.py
@@ -11,6 +11,9 @@ from datetime import datetime
 from subprocess import call, check_output
 import sys
 import os
+import tag_lists
+import csv
+
 
 class GitTag():
     """Represents a Tag in a git repository"""
@@ -25,7 +28,9 @@ class GitTag():
         self.tag_timestamp = float(raw_timestamp.strip())
 
         # process the datetime
-        self.date = raw_date.strip()
+        self.date = datetime.fromtimestamp(float(raw_timestamp.strip()))
+
+        self.date_as_str = raw_date
 
         raw_tag = raw_tag.split("tag:")[1]
         # get the git-tag
@@ -34,14 +39,60 @@ class GitTag():
         else:
             self.name = raw_tag.replace(")", "").strip()
 
+    def __str__(self):
+        return str({'name': self.name, 'date': str(self.date)})
 
 
-    def date_to_string(self, time):
-        """Returns: A string representation of a UNIX timestamp"""
-        return str(datetime.fromtimestamp(time))
+class GitCommit():
+    """Represents a commit in a git repository"""
+
+    bug_related_words = ["bug", "fix", "defect", "broken", "crash", "freeze", "break", "wrong", "glitch", "proper"]
+    line_separator = "<commit_data_separator>"
+    entry_separator = "<commit_entry_separator>"
+
+    @staticmethod
+    def get_commits_command(git_repo):
+        get_commits_command = 'cd {0}; git log --pretty="format:' \
+                              '%H<commit_data_separator>' \
+                              '%ai<commit_data_separator>' \
+                              '%at<commit_data_separator>' \
+                              '%b<commit_entry_separator>"'.format(git_repo)
+
+        return get_commits_command
+
+    def __init__(self, line, repo):
+        raw_hash, raw_date, raw_timestamp, raw_body = line.split(GitCommit.line_separator)
+
+        self.hash = raw_hash.strip()
+        self.date = datetime.fromtimestamp(float(raw_timestamp))
+        self.timestamp = float(raw_timestamp)
+        self.date_as_str = raw_date
+        self.message = raw_body
+
+        self.repo = repo
+
+    def get_touched_files(self):
+        # This git command returns:
+        # "\n
+        # <added_lines>\t<deleted_lines>\t<file_name>\n
+        # <added_lines>\t<deleted_lines>\t<file_name>"
+        cmd_show_numstat = "cd {0}; git show --numstat --format='format:' {1}".format(self.repo.repo_path, self.hash)
+        commit_stats = check_output(cmd_show_numstat, shell=True).decode("utf-8")
+
+        touched_files = []
+
+        for file_stats in commit_stats.split("\n"):
+            if len(file_stats.split("\t")) == 3:  # Process only if it's a line with actual data
+                added_lines, deleted_lines, file_name = file_stats.split("\t")
+                touched_files.append(file_name)
+
+        return touched_files
+
+    def is_bug_related(self):
+        return any([word.upper() in self.message.upper() for word in GitCommit.bug_related_words])
 
     def __str__(self):
-        return str({'name': self.name, 'date': self.date})
+        return str({'hash': self.hash, 'date': str(self.date)})
 
 
 class GitRepository():
@@ -56,17 +107,58 @@ class GitRepository():
         """Returns: List of all the tags in the repository"""
 
         cmd_get_tags = 'cd {0}; git log --tags --simplify-by-decoration --pretty="format:%d | %H | %ai | %at" |grep "tag:"'.format(self.repo_path)
-        results_cmd = check_output(cmd_get_tags, shell=True).decode("utf-8")
+        results = check_output(cmd_get_tags, shell=True).decode("utf-8")
 
-        tags = [GitTag(str(line)) for line in results_cmd.splitlines()]
-
+        tags = [GitTag(str(line)) for line in results.splitlines()]
         tags.sort(key=lambda t: t.date)
+        # tags = self.sort_tags(tags)
 
         return tags
 
+    def get_commits(self):
+        """Returns: List of all the commits in the repository"""
+
+        cmd_get_commits = GitCommit.get_commits_command(self.repo_path)
+        results = check_output(cmd_get_commits, shell=True).decode("utf-8")
+
+        commits = [GitCommit(str(line), self) for line in results.split(GitCommit.entry_separator) if line]
+        commits.sort(key=lambda t: t.date)
+
+        return commits
+
+    def sort_tags(self, tags):
+        result = []
+        ordered_tags = self.get_interesting_releases()
+
+        for tag in ordered_tags:
+            result.append([t for t in tags if t.name == tag][0])
+
+        return result
+    
+    def get_interesting_releases(self):
+        ordered_tags = []
+    
+        if 'apache-avro' in self.git_repo:
+            ordered_tags = tag_lists.apache_avro_releases
+        elif 'apache-mahout' in self.git_repo:
+            ordered_tags = tag_lists.apache_mahout_releases
+        elif 'apache-tika' in self.git_repo:
+            ordered_tags = tag_lists.apache_tika_releases
+        elif 'vrapper' in self.git_repo:
+            ordered_tags = tag_lists.vrapper_releases
+        elif 'apache-zookeeper' in self.git_repo:
+            ordered_tags = tag_lists.apache_zookeeper_releases
+        elif 'facebook-android-sdk' in self.git_repo:
+            ordered_tags = tag_lists.facebook_android_sdk_releases
+        elif 'github-android-app' in self.git_repo:
+            ordered_tags = tag_lists.github_android_app_releases
+        elif 'wordpress-android' in self.git_repo:
+            ordered_tags = tag_lists.wordpress_android_app
+    
+        return ordered_tags
+
 
 get_bug_commit_per_file_executable = "get_bug_commit_ratio_per_file.py"
-default_bug_commit_output_dir = "/home/kevin/Desktop/evolution-project/"
 
 
 def extract_bug_commit_data(git_repo, tags, output_dir):
@@ -88,26 +180,66 @@ def extract_bug_commit_data(git_repo, tags, output_dir):
     call(["git", "checkout", "master"])
 
 
-def main(git_repo, output_dir):
+def reverse(l):
+    return l[::-1]
+
+
+def first_or_default(l, default=None):
+    if l:
+        return l[0]
+    else:
+        return default
+
+
+csv_header = ['file_name', 'release', 'commit_num', 'bug_commit_num']  # 'bug_commit_ratio'
+
+
+def main(args):
+    git_repo = args[1]
+    output_file = args[2]
 
     repo = GitRepository(git_repo)
+    commits = repo.get_commits()
     tags = repo.get_tags()
 
-    try:
-        os.makedirs(output_dir)
-    except FileExistsError as ex:
-        pass
+    result = []
+    initial_date = None
 
-    # Use this to run process in a single thread.
-    extract_bug_commit_data(git_repo, tags, output_dir)
+    for tag in tags:
+        final_date = tag.date
+
+        if initial_date:
+            commits_in_tag = [c for c in commits if initial_date < c.date <= final_date]
+        else:
+            commits_in_tag = [c for c in commits if c.date <= final_date]
+
+        initial_date = final_date
+
+        for commit in commits_in_tag:
+            touched_files = commit.get_touched_files()
+            bug_related = commit.is_bug_related()
+
+            for file in touched_files:
+                file_data = first_or_default([f for f in result if f['release'] == tag.name and f['file_name'] == file])
+
+                if file_data:
+                    file_data['commit_num'] += 1
+                    if bug_related:
+                        file_data['bug_commit_num'] += 1
+                else:
+                    result.append({'file_name': file,
+                                   'release': tag.name,
+                                   'commit_num': 1,
+                                   'bug_commit_num': 1 if bug_related else 0})
+
+    # for entry in result:
+    #     entry['bug_commit_ratio'] = entry['bug_commit_num'] / entry['commit_num']
+
+    with open(output_file, 'w', newline='') as output_file:
+        writer = csv.DictWriter(output_file, csv_header)
+        writer.writeheader()
+        writer.writerows(result)
 
 
 if __name__ == '__main__':
-    repo_path = sys.argv[1]
-
-    output_dir = default_bug_commit_output_dir
-    if len(sys.argv) > 2:
-        output_dir = sys.argv[2]
-
-    main(repo_path, output_dir)
-    # main("/home/kevin/Desktop/facebook-android-sdk/")
+    main(sys.argv)
